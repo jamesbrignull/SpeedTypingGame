202e255b63da5b58d1c92545aaabdb93b04e8acc
jamesbrignull <jamesbrignull@gmail.com>
2020-10-09 14:26:22 +0100

    applied start game functionality

--- speedtypinggame2020/src/App.js
+++ speedtypinggame2020/src/App.js
@@ -3,25 +3,28 @@ import './App.css';
 
 function App() {
   const [text, setText] = useState('')
-  const [time, setTime] = useState(10)
+  const [time, setTime] = useState(3)
+  const [isTimeRun, setIsTimeRun] = useState(false)
 
   function handleChange(event) {
     const { value } = event.target
     setText(value)
     }
   
-  function countWords(str) {
-    const wordsArray = str.trim().split(' ')
-    return wordsArray.filter(word => word !== '').length
-  }
+  // function countWords(str) {
+  //   const wordsArray = str.trim().split(' ')
+  //   return wordsArray.filter(word => word !== '').length
+  // }
 
   useEffect(() => {
-    if (time > 0) {
+    if (isTimeRun && time > 0) {
       setTimeout(() => {
           setTime(prevTime => prevTime - 1)
       }, 1000)
+    } else if (time === 0) {
+        setIsTimeRun(false)
     }
-}, [time])
+}, [time, isTimeRun])
 
 console.log(text)
     return (
@@ -33,7 +36,7 @@ console.log(text)
         </textarea>
         <h4>Time remaining: {time}</h4>
         <button 
-          onClick={() => countWords(text)}
+          onClick={() => setIsTimeRun(true)}
         >Start game
         </button>
         <h1>Word count</h1>
